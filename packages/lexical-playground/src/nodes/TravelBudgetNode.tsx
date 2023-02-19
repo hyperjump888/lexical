55697470f6b347614a2bdf34a987a1710ce84d73
dickyj <dickyj@injani.com>
2023-02-19 17:57:47 +0700

    travelbudgetnode

---
+++ packages/lexical-playground/src/nodes/TravelBudgetNode.tsx
@@ -0,0 +1,383 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {
+  DecoratorNode,
+  DOMConversionMap,
+  DOMConversionOutput,
+  DOMExportOutput,
+  LexicalNode,
+  NodeKey,
+  SerializedLexicalNode,
+  Spread,
+} from 'lexical';
+import * as React from 'react';
+import {Suspense} from 'react';
+
+export type Options = ReadonlyArray<Option>;
+
+export type Option = Readonly<{
+  text: string;
+  uid: string;
+  votes: Array<number>;
+}>;
+
+// list of world currencies in array format, with the currency code as the key and name as the value
+const currencyList: { code: string, name: string }[] = [
+  { code: 'AED', name: 'United Arab Emirates Dirham' },
+  { code: 'AFA', name: 'Afghan Afghani' },
+  { code: 'ALL', name: 'Albanian Lek' },
+  { code: 'AMD', name: 'Armenian Dram' },
+  { code: 'ANG', name: 'Netherlands Antillean Guilder' },
+  { code: 'AOA', name: 'Angolan Kwanza' },
+  { code: 'ARS', name: 'Argentine Peso' },
+  { code: 'AUD', name: 'Australian Dollar' },
+  { code: 'AWG', name: 'Aruban Florin' },
+  { code: 'AZN', name: 'Azerbaijani Manat' },
+  { code: 'BAM', name: 'Bosnia-Herzegovina Convertible Mark' },
+  { code: 'BBD', name: 'Barbadian Dollar' },
+  { code: 'BDT', name: 'Bangladeshi Taka' },
+  { code: 'BEF', name: 'Belgian Franc' },
+  { code: 'BGN', name: 'Bulgarian Lev' },
+  { code: 'BHD', name: 'Bahraini Dinar' },
+  { code: 'BIF', name: 'Burundian Franc' },
+  { code: 'BMD', name: 'Bermudan Dollar' },
+  { code: 'BND', name: 'Brunei Dollar' },
+  { code: 'BOB', name: 'Bolivian Boliviano' },
+  { code: 'BRL', name: 'Brazilian Real' },
+  { code: 'BSD', name: 'Bahamian Dollar' },
+  { code: 'BTC', name: 'Bitcoin' },
+  { code: 'BTN', name: 'Bhutanese Ngultrum' },
+  { code: 'BWP', name: 'Botswanan Pula' },
+  { code: 'BYR', name: 'Belarusian Ruble' },
+  { code: 'BZD', name: 'Belize Dollar' },
+  { code: 'CAD', name: 'Canadian Dollar' },
+  { code: 'CDF', name: 'Congolese Franc' },
+  { code: 'CHF', name: 'Swiss Franc' },
+  { code: 'CLP', name: 'Chilean Peso' },
+  { code: 'CNY', name: 'Chinese Yuan' },
+  { code: 'COP', name: 'Colombian Peso' },
+  { code: 'CRC', name: 'Costa Rican ColÃ³n' },
+  { code: 'CUC', name: 'Cuban Convertible Peso' },
+  { code: 'CVE', name: 'Cape Verdean Escudo' },
+  { code: 'CZK', name: 'Czech Republic Koruna' },
+  { code: 'DEM', name: 'German Mark' },
+  { code: 'DJF', name: 'Djiboutian Franc' },
+  { code: 'DKK', name: 'Danish Krone' },
+  { code: 'DOP', name: 'Dominican Peso' },
+  { code: 'DZD', name: 'Algerian Dinar' },
+  { code: 'EEK', name: 'Estonian Kroon' },
+  { code: 'EGP', name: 'Egyptian Pound' },
+  { code: 'ERN', name: 'Eritrean Nakfa' },
+  { code: 'ETB', name: 'Ethiopian Birr' },
+  { code: 'EUR', name: 'Euro' },
+  { code: 'FJD', name: 'Fijian Dollar' },
+  { code: 'FKP', name: 'Falkland Islands Pound' },
+  { code: 'GBP', name: 'British Pound Sterling' },
+  { code: 'GEL', name: 'Georgian Lari' },
+  { code: 'GHS', name: 'Ghanaian Cedi' },
+  { code: 'GIP', name: 'Gibraltar Pound' },
+  { code: 'GMD', name: 'Gambian Dalasi' },
+  { code: 'GNF', name: 'Guinean Franc' },
+  { code: 'GRD', name: 'Greek Drachma' },
+  { code: 'GTQ', name: 'Guatemalan Quetzal' },
+  { code: 'GYD', name: 'Guyanaese Dollar' },
+  { code: 'HKD', name: 'Hong Kong Dollar' },
+  { code: 'HNL', name: 'Honduran Lempira' },
+  { code: 'HRK', name: 'Croatian Kuna' },
+  { code: 'HTG', name: 'Haitian Gourde' },
+  { code: 'HUF', name: 'Hungarian Forint' },
+  { code: 'IDR', name: 'Indonesian Rupiah' },
+  { code: 'ILS', name: 'Israeli New Sheqel' },
+  { code: 'INR', name: 'Indian Rupee' },
+  { code: 'IQD', name: 'Iraqi Dinar' },
+  { code: 'IRR', name: 'Iranian Rial' },
+  { code: 'ISK', name: 'Icelandic KrÃ³na' },
+  { code: 'ITL', name: 'Italian Lira' },
+  { code: 'JMD', name: 'Jamaican Dollar' },
+  { code: 'JOD', name: 'Jordanian Dinar' },
+  { code: 'JPY', name: 'Japanese Yen' },
+  { code: 'KES', name: 'Kenyan Shilling' },
+  { code: 'KGS', name: 'Kyrgystani Som' },
+  { code: 'KHR', name: 'Cambodian Riel' },
+  { code: 'KMF', name: 'Comorian Franc' },
+  { code: 'KPW', name: 'North Korean Won' },
+  { code: 'KRW', name: 'South Korean Won' },
+  { code: 'KWD', name: 'Kuwaiti Dinar' },
+  { code: 'KYD', name: 'Cayman Islands Dollar' },
+  { code: 'KZT', name: 'Kazakhstani Tenge' },
+  { code: 'LAK', name: 'Laotian Kip' },
+  { code: 'LBP', name: 'Lebanese Pound' },
+  { code: 'LKR', name: 'Sri Lankan Rupee' },
+  { code: 'LRD', name: 'Liberian Dollar' },
+  { code: 'LSL', name: 'Lesotho Loti' },
+  { code: 'LTL', name: 'Lithuanian Litas' },
+  { code: 'LVL', name: 'Latvian Lats' },
+  { code: 'LYD', name: 'Libyan Dinar' },
+  { code: 'MAD', name: 'Moroccan Dirham' },
+  { code: 'MDL', name: 'Moldovan Leu' },
+  { code: 'MGA', name: 'Malagasy Ariary' },
+  { code: 'MKD', name: 'Macedonian Denar' },
+  { code: 'MMK', name: 'Myanmar Kyat' },
+  { code: 'MNT', name: 'Mongolian Tugrik' },
+  { code: 'MOP', name: 'Macanese Pataca' },
+  { code: 'MRO', name: 'Mauritanian Ouguiya' },
+  { code: 'MUR', name: 'Mauritian Rupee' },
+  { code: 'MVR', name: 'Maldivian Rufiyaa' },
+  { code: 'MWK', name: 'Malawian Kwacha' },
+  { code: 'MXN', name: 'Mexican Peso' },
+  { code: 'MYR', name: 'Malaysian Ringgit' },
+  { code: 'MZM', name: 'Mozambican Metical' },
+  { code: 'NAD', name: 'Namibian Dollar' },
+  { code: 'NGN', name: 'Nigerian Naira' },
+  { code: 'NIO', name: 'Nicaraguan CÃ³rdoba' },
+  { code: 'NOK', name: 'Norwegian Krone' },
+  { code: 'NPR', name: 'Nepalese Rupee' },
+  { code: 'NZD', name: 'New Zealand Dollar' },
+  { code: 'OMR', name: 'Omani Rial' },
+  { code: 'PAB', name: 'Panamanian Balboa' },
+  { code: 'PEN', name: 'Peruvian Nuevo Sol' },
+  { code: 'PGK', name: 'Papua New Guinean Kina' },
+  { code: 'PHP', name: 'Philippine Peso' },
+  { code: 'PKR', name: 'Pakistani Rupee' },
+  { code: 'PLN', name: 'Polish Zloty' },
+  { code: 'PYG', name: 'Paraguayan Guarani' },
+  { code: 'QAR', name: 'Qatari Rial' },
+  { code: 'RON', name: 'Romanian Leu' },
+  { code: 'RSD', name: 'Serbian Dinar' },
+  { code: 'RUB', name: 'Russian Ruble' },
+  { code: 'RWF', name: 'Rwandan Franc' },
+  { code: 'SAR', name: 'Saudi Riyal' },
+  { code: 'SBD', name: 'Solomon Islands Dollar' },
+  { code: 'SCR', name: 'Seychellois Rupee' },
+  { code: 'SDG', name: 'Sudanese Pound' },
+  { code: 'SEK', name: 'Swedish Krona' },
+  { code: 'SGD', name: 'Singapore Dollar' },
+  { code: 'SHP', name: 'St. Helena Pound' },
+  { code: 'SKK', name: 'Slovak Koruna' },
+  { code: 'SLL', name: 'Sierra Leonean Leone' },
+  { code: 'SOS', name: 'Somali Shilling' },
+  { code: 'SRD', name: 'Surinamese Dollar' },
+  { code: 'STD', name: 'São Tomé and Príncipe Dobra' },
+  { code: 'SVC', name: 'Salvadoran ColÃ³n' },
+  { code: 'SYP', name: 'Syrian Pound' },
+  { code: 'SZL', name: 'Swazi Lilangeni' },
+  { code: 'THB', name: 'Thai Baht' },
+  { code: 'TJS', name: 'Tajikistani Somoni' },
+  { code: 'TMT', name: 'Turkmenistani Manat' },
+  { code: 'TND', name: 'Tunisian Dinar' },
+  { code: 'TOP', name: "Tongan Pa'anga" },
+  { code: 'TRY', name: 'Turkish Lira' },
+  { code: 'TTD', name: 'Trinidad & Tobago Dollar' },
+  { code: 'TWD', name: 'New Taiwan Dollar' },
+  { code: 'TZS', name: 'Tanzanian Shilling' },
+  { code: 'UAH', name: 'Ukrainian Hryvnia' },
+  { code: 'UGX', name: 'Ugandan Shilling' },
+  { code: 'USD', name: 'US Dollar' },
+  { code: 'UYU', name: 'Uruguayan Peso' },
+  { code: 'UZS', name: 'Uzbekistan Som' },
+  { code: 'VEF', name: 'Venezuelan BolÃvar' },
+  { code: 'VND', name: 'Vietnamese Dong' },
+  { code: 'VUV', name: 'Vanuatu Vatu' },
+  { code: 'WST', name: 'Samoan Tala' },
+  { code: 'XAF', name: 'CFA Franc BEAC' },
+  { code: 'XCD', name: 'East Caribbean Dollar' },
+  { code: 'XDR', name: 'Special Drawing Rights' },
+  { code: 'XOF', name: 'CFA Franc BCEAO' },
+  { code: 'XPF', name: 'CFP Franc' },
+  { code: 'YER', name: 'Yemeni Rial' },
+  { code: 'ZAR', name: 'South African Rand' },
+  { code: 'ZMK', name: 'Zambian Kwacha' }
+];
+
+// list of categories ['Accommodation','Transportation'] in array format
+const categories: string[] = ['Accommodation', 'Transportation'];
+  
+  
+
+
+function createUID(): string {
+  return Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 5);
+}
+
+export function createPollOption(text = ''): Option {
+  return {
+    text,
+    uid: createUID(),
+    votes: [],
+  };
+}
+
+function cloneOption(
+  option: Option,
+  text: string,
+  votes?: Array<number>,
+): Option {
+  return {
+    text,
+    uid: option.uid,
+    votes: votes || Array.from(option.votes),
+  };
+}
+
+export type SerializedPollNode = Spread<
+  {
+    question: string;
+    options: Options;
+    type: 'poll';
+    version: 1;
+  },
+  SerializedLexicalNode
+>;
+
+function convertPollElement(domNode: HTMLElement): DOMConversionOutput | null {
+  const question = domNode.getAttribute('data-lexical-poll-question');
+  if (question !== null) {
+    const node = $createPollNode(question);
+    return {node};
+  }
+  return null;
+}
+
+export class PollNode extends DecoratorNode<JSX.Element> {
+  __question: string;
+  __options: Options;
+
+  static getType(): string {
+    return 'poll';
+  }
+
+  static clone(node: PollNode): PollNode {
+    return new PollNode(node.__question, node.__options, node.__key);
+  }
+
+  static importJSON(serializedNode: SerializedPollNode): PollNode {
+    const node = new PollNode(serializedNode.question,serializedNode.options)
+    return node
+    // const node = $createPollNode(serializedNode.question);
+    // try {
+    //   serializedNode.options.forEach(node.addOption);
+    //   return node;
+    // } catch (error) {
+    //   console.error('import json failed')
+    //   return node
+    // }
+
+  }
+
+  constructor(question: string, options?: Options, key?: NodeKey) {
+    super(key);
+    this.__question = question;
+    this.__options = options || [createPollOption(), createPollOption()];
+  }
+
+  exportJSON(): SerializedPollNode {
+    return {
+      options: this.__options,
+      question: this.__question,
+      type: 'poll',
+      version: 1,
+    };
+  }
+
+  addOption(option: Option): void {
+    const self = this.getWritable();
+    const options = Array.from(self.__options);
+    options.push(option);
+    self.__options = options;
+  }
+
+  deleteOption(option: Option): void {
+    const self = this.getWritable();
+    const options = Array.from(self.__options);
+    const index = options.indexOf(option);
+    options.splice(index, 1);
+    self.__options = options;
+  }
+
+  setOptionText(option: Option, text: string): void {
+    const self = this.getWritable();
+    const clonedOption = cloneOption(option, text);
+    const options = Array.from(self.__options);
+    const index = options.indexOf(option);
+    options[index] = clonedOption;
+    self.__options = options;
+  }
+
+  toggleVote(option: Option, clientID: number): void {
+    const self = this.getWritable();
+    const votes = option.votes;
+    const votesClone = Array.from(votes);
+    const voteIndex = votes.indexOf(clientID);
+    if (voteIndex === -1) {
+      votesClone.push(clientID);
+    } else {
+      votesClone.splice(voteIndex, 1);
+    }
+    const clonedOption = cloneOption(option, option.text, votesClone);
+    const options = Array.from(self.__options);
+    const index = options.indexOf(option);
+    options[index] = clonedOption;
+    self.__options = options;
+  }
+
+  static importDOM(): DOMConversionMap | null {
+    return {
+      span: (domNode: HTMLElement) => {
+        if (!domNode.hasAttribute('data-lexical-poll-question')) {
+          return null;
+        }
+        return {
+          conversion: convertPollElement,
+          priority: 2,
+        };
+      },
+    };
+  }
+
+  exportDOM(): DOMExportOutput {
+    const element = document.createElement('span');
+    element.setAttribute('data-lexical-poll-question', this.__question);
+    return {element};
+  }
+
+  createDOM(): HTMLElement {
+    const elem = document.createElement('span');
+    elem.style.display = 'inline-block';
+    return elem;
+  }
+
+  updateDOM(): false {
+    return false;
+  }
+
+  decorate(): JSX.Element {
+    return (
+      <Suspense fallback={null}>
+        <PollComponent
+          question={this.__question}
+          options={this.__options}
+          nodeKey={this.__key}
+        />
+      </Suspense>
+    );
+  }
+}
+
+export function $createPollNode(question: string): PollNode {
+  return new PollNode(question);
+}
+
+export function $isPollNode(
+  node: LexicalNode | null | undefined,
+): node is PollNode {
+  return node instanceof PollNode;
+}
